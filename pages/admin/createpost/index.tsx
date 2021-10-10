f36ebdc4e38d5d584d7638e035466c58039a4911
Tanjid <tamjidahammad10@gmail.com>
2021-10-11 00:23:25 +0600

    minor fix

--- pages/admin/createpost/index.tsx
+++ pages/admin/createpost/index.tsx
@@ -72,6 +72,13 @@ const CreatePost: React.FC<Props> = () => {
   }
   if (checkSignedIn == "") {
     router.push("/admin");
+    return (
+      <div>
+        <div className=" flex justify-center items-center">
+          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-purple-500" />
+        </div>
+      </div>
+    );
   }
 
   return (
