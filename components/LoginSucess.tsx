4cd42a5fb6af6060de4d83f21daa07f8e830e66d
Tanjid <tamjidahammad10@gmail.com>
2021-10-10 11:14:13 +0600

    adding login

---
+++ components/LoginSucess.tsx
@@ -0,0 +1,26 @@
+const LoginSucess = ({ posts }) => {
+  return (
+    <div>
+      <div className="card lg:card-side bordered">
+        <figure>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src="https://picsum.photos/id/1005/400/250" alt="nothing" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Horizontal</h2>
+          <p>
+            Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit
+            sit necessitatibus veritatis sed molestiae voluptates incidunt iure
+            sapiente.
+          </p>
+          <div className="card-actions">
+            <button className="btn btn-primary">Delete</button>
+            <button className="btn btn-ghost">Edit</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginSucess;
---
+++ pages/admin/index.tsx
@@ -0,0 +1,105 @@
+import { useEffect, useState } from "react";
+import {
+  onAuthStateChanged,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import app from "../../config/fire";
+import LoginSucess from "../../components/LoginSucess";
+import { useForm } from "react-hook-form";
+import getAllThePosts from "../../functions/getAllThePosts";
+
+interface Props {
+  posts: any;
+}
+
+const Admin: React.FC<Props> = ({posts}) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [checkSignedIn, setCheckSignedIn]: any = useState("");
+  const auth = getAuth(app);
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: any) => {
+    signInWithEmailAndPassword(auth, data.email, data.password)
+      .then(() => {
+        console.log("success");
+      })
+      .catch((error) => {
+        console.log("Wrong Authentication");
+      });
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setCheckSignedIn(user);
+        console.log("user", user);
+      } else {
+        setCheckSignedIn("");
+        console.log("not signed in");
+      }
+      setIsLoading(false);
+    });
+  }, [auth]);
+
+  return (
+    <div>
+      {checkSignedIn ? (
+        <>
+          <LoginSucess posts={posts}/>
+        </>
+      ) : isLoading ? (
+        <>
+          <div className=" flex justify-center items-center">
+            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-purple-500" />
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="p-10 card bg-base-200">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter Email Adress"
+                  className="input input-info input-bordered"
+                  {...register("email", { required: true })}
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Enter Password"
+                  className="input input-info input-bordered"
+                  {...register("password", { required: true })}
+                />
+              </div>
+              <div className="form-control mt-5">
+                <button className="btn btn-active w-24" type="submit">
+                  Sign In
+                </button>
+              </div>
+            </form>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export async function getServerSideProps() {
+  const posts = await getAllThePosts();
+  return {
+    props: { posts },
+  };
+}
+
+export default Admin;
